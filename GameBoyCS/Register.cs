c9f1b17b0c4cb4f89853b70a2561c01fd0968b4f
Guilherme Martin <Guilherme Martin@DESKTOP-ELAM2RV>
2021-10-04 19:10:43 -0300

    base init

---
+++ GameBoyCS/Register.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GameBoyCS
+{
+    public struct Register
+    {
+        public ushort m, t;
+        public ushort pc, sp;
+        public byte a, b, c, d, e, f;
+    }
+}
